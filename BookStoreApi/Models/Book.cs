8ce03f79f0e404dfd0ad8aff3f684d4bc9f8aee0
Ratul Ali <rahim498617@gmail.com>
2022-04-05 12:54:13 +0600

    Updated APIs

--- BookStoreApi/Models/Book.cs
+++ BookStoreApi/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
 namespace BookStoreApi.Models
 {
@@ -9,6 +10,7 @@ namespace BookStoreApi.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
         [BsonElement("Name")]
+        [JsonPropertyName("Name")]
         public string BookName { get; set; } = null!;
         public decimal Price { get; set; }
         public string Category { get; set; } = null!;
--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the (DI) container.
 builder.Services.Configure<BookStoreDatabaseSettings>(builder.Configuration.GetSection("BookStoreDatabase"));
 builder.Services.AddSingleton<BooksService>();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
